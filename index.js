2ce663f1c044e2134684d9567a45d3b6a9989375
ronxjansen <ronxjansen@gmail.com>
2015-07-23 11:17:26 +0200

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Model from 'src/lib/Model';
-import Component from 'src/lib/Component';
+import Model from './src/lib/Model';
+import Component from './src/lib/Component';
 
 export {Model, Component};
