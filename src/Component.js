06cc22b3d78a64cd576406035fec2f1f76e72523
ronxjansen <ronxjansen@gmail.com>
2015-07-23 12:40:31 +0200

    Fixed paths in Component

--- src/Component.js
+++ src/Component.js
@@ -1,9 +1,9 @@
-import events from './../../bower_components/utilbase/src/events';
-import pubsub from './../../bower_components/utilbase/src/pubsub';
-import utils from './../../bower_components/utilbase/src/bind';
-// import log from './../../bower_components/logger';
-import createEl from './../../bower_components/utilbase/src/DOM';
-// import Flipbase from './../../bower_components/global';
+import events from './../bower_components/utilbase/src/events';
+import pubsub from './../bower_components/utilbase/src/pubsub';
+import utils from './../bower_components/utilbase/src/bind';
+// import log from './../bower_components/logger';
+import createEl from './../bower_components/utilbase/src/DOM';
+// import Flipbase from './../bower_components/global';
 
 /**
  * @example
