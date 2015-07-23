ca4277ca1b823d9c151c6422d476bb52013cf01c
ronxjansen <ronxjansen@gmail.com>
2015-07-23 12:53:07 +0200

    Fixed bins

--- src/Component.js
+++ src/Component.js
@@ -1,6 +1,6 @@
 import events from './../bower_components/utilbase/src/events';
 import pubsub from './../bower_components/utilbase/src/pubsub';
-import utils from './../bower_components/utilbase/src/bind';
+import {bind} from './../bower_components/utilbase/src/bind';
 // import log from './../bower_components/logger';
 import {createEl} from './../bower_components/utilbase/src/DOM';
 import Flipbase from './../bower_components/utilbase/src/namespace';
