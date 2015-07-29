eccd1fe72f7404d7e5803d591188d50ce72c02e6
ronxjansen <ronxjansen@gmail.com>
2015-07-29 12:05:50 +0200

    Improved log lib

--- src/utils/logx.js
+++ src/utils/logx.js
@@ -43,19 +43,20 @@ export function remove(index) {
  * @type {Object}
  */
 var colors = {
-  error: '#FF0000',
-  info: '#AABB0011',
-  debug: '#00A0FF',
-  warn: '#DEA201'
+  error: '#e11e1e', // red
+  info: '#0090ff', // blue
+  debug: '#ff6000', // dark orange
+  warn: '#ff0090' // pink
 };
+
+if (!window.console) window.console = {};
+if (!window.console.log) window.console.log = function () {};
 
 export function consoleTransport(level, msg, meta) {
   var color = colors[level] || '#000000';
   var style = 'color: ' + color + ';';
 
   if(meta && JSON) msg += ', meta: ' + JSON.stringify(meta);
-
-  if (console[level]) return console[level](msg);
 
   if (hasConsoleStyling()) return console.log('%c ' + msg, style);
 
