4ea9fd3aea36aa97f9bca9eba687baa0643c51f8
ronxjansen <ronxjansen@gmail.com>
2015-12-14 11:54:08 +0100

    Fixed clone method

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -39,13 +39,55 @@ var u = {
    * @param  {object} obj
    * @return {object}
    */
-  clone: function (obj) {
-    if (!obj || typeof obj !== 'object')
-      return obj;
+  clone: function (src) {
+    function mixin (dest, source, copyFunc) {
+      var name, s, i, empty = {};
+      for (name in source) {
+        // the (!(name in empty) || empty[name] !== s) condition avoids copying properties in "source"
+        // inherited from Object.prototype.  For example, if dest has a custom toString() method,
+        // don't overwrite it with the toString() method that source inherited from Object.prototype
+        s = source[name];
+        if (!(name in dest) || (dest[name] !== s && (!(name in empty) || empty[name] !== s))) {
+          dest[name] = copyFunc ? copyFunc(s) : s;
+        }
+      }
+      return dest;
+    }
 
-    var copy = {};
-    copy = u.assign(copy, obj);
-    return copy;
+    if (!src || typeof src != 'object' || Object.prototype.toString.call(src) === '[object Function]') {
+      // null, undefined, any non-object, or function
+      return src; // anything
+    }
+    if (src.nodeType && 'cloneNode' in src) {
+      // DOM Node
+      return src.cloneNode(true); // Node
+    }
+    if (src instanceof Date) {
+      // Date
+      return new Date(src.getTime()); // Date
+    }
+    if (src instanceof RegExp) {
+      // RegExp
+      return new RegExp(src);   // RegExp
+    }
+    var copy, i, l;
+    if (src instanceof Array) {
+      // array
+      copy = [];
+      for(i = 0, l = src.length; i < l; ++i){
+        if(i in src){
+          copy.push(clone(src[i]));
+        }
+      }
+      // we don't clone functions for performance reasons
+      //    }else if(d.isFunction(src)){
+      //      // function
+      //      copy = function(){ return src.apply(this, arguments); };
+    } else {
+      // generic objects
+      copy = src.constructor ? new src.constructor() : {};
+    }
+    return mixin(copy, src, u.clone);
   },
 
   /**
